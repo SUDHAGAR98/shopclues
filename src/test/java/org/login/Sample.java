3da7007c607850f61eb0f456b6de9ddce6179a7d
SUDHAGAR98 <sudhagar20398@gmail.com>
2023-01-23 20:09:27 +0530

    second branch

--- src/test/java/org/login/Sample.java
+++ src/test/java/org/login/Sample.java
@@ -7,5 +7,12 @@ System.out.println("first");
 private void m2() {
 System.out.println("second");
 }
+private void m3() {
+	System.out.println("three");
+
+}
+private void m4() {
+System.out.println("four");
+}
 	
 }
